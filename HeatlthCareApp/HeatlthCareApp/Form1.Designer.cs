2b89544af5c24ede6e4e815e7fa8b4d4ccd047bc
Joekamau91 <Joekamau91@users.noreply.github.com>
2019-05-27 15:41:29 -0400

    Add files via upload

---
+++ HeatlthCareApp/HeatlthCareApp/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace HeatlthCareApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPatientId = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.dgvPatientInfo = new System.Windows.Forms.DataGridView();
+            this.fname = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPatientInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Patient Id";
+            // 
+            // txtPatientId
+            // 
+            this.txtPatientId.Location = new System.Drawing.Point(241, 53);
+            this.txtPatientId.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPatientId.Name = "txtPatientId";
+            this.txtPatientId.Size = new System.Drawing.Size(279, 30);
+            this.txtPatientId.TabIndex = 1;
+            this.txtPatientId.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.Lime;
+            this.btnInsert.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnInsert.Location = new System.Drawing.Point(53, 479);
+            this.btnInsert.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(155, 51);
+            this.btnInsert.TabIndex = 2;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // dgvPatientInfo
+            // 
+            this.dgvPatientInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPatientInfo.Location = new System.Drawing.Point(585, 100);
+            this.dgvPatientInfo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvPatientInfo.Name = "dgvPatientInfo";
+            this.dgvPatientInfo.ReadOnly = true;
+            this.dgvPatientInfo.RowTemplate.Height = 28;
+            this.dgvPatientInfo.Size = new System.Drawing.Size(828, 345);
+            this.dgvPatientInfo.TabIndex = 3;
+            this.dgvPatientInfo.ReadOnlyChanged += new System.EventHandler(this.dgvPatientInfo_ReadOnlyChanged);
+            this.dgvPatientInfo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dgvPatientInfo.DoubleClick += new System.EventHandler(this.dgvPatientInfo_DoubleClick);
+            // 
+            // fname
+            // 
+            this.fname.AutoSize = true;
+            this.fname.Location = new System.Drawing.Point(49, 122);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(100, 24);
+            this.fname.TabIndex = 4;
+            this.fname.Text = "First Name";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(241, 113);
+            this.txtFirstName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(279, 30);
+            this.txtFirstName.TabIndex = 5;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(241, 177);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(279, 30);
+            this.txtLastName.TabIndex = 6;
+            this.txtLastName.TextChanged += new System.EventHandler(this.txtLastName_TextChanged);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(241, 236);
+            this.txtAge.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(279, 30);
+            this.txtAge.TabIndex = 7;
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(241, 304);
+            this.txtStreet.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(279, 30);
+            this.txtStreet.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Age";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Street";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(241, 353);
+            this.txtCity.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(279, 30);
+            this.txtCity.TabIndex = 12;
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Location = new System.Drawing.Point(241, 415);
+            this.txtZipCode.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(279, 30);
+            this.txtZipCode.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 360);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 24);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "City";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(49, 423);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 24);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Zip Code";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(234, 483);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(129, 43);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Yellow;
+            this.btnReset.Location = new System.Drawing.Point(385, 487);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(135, 43);
+            this.btnReset.TabIndex = 17;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(646, 36);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(279, 30);
+            this.txtSearch.TabIndex = 18;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(164)))), ((int)(((byte)(250)))));
+            this.btnSearch.Location = new System.Drawing.Point(1133, 30);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(135, 42);
+            this.btnSearch.TabIndex = 19;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(162)))), ((int)(((byte)(198)))));
+            this.ClientSize = new System.Drawing.Size(1445, 756);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtStreet);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.dgvPatientInfo);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtPatientId);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "MyHealthApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPatientInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPatientId;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.DataGridView dgvPatientInfo;
+        private System.Windows.Forms.Label fname;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ HeatlthCareApp/HeatlthCareApp/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace HeatlthCareApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=localhost;Initial Catalog=master;Integrated Security=True");
+        int PatientId = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLastName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                    if (btnInsert.Text =="Insert") {
+                        SqlCommand sqlCmd = new SqlCommand("Insert", sqlCon);
+                        sqlCmd.CommandType = CommandType.StoredProcedure;
+                        sqlCmd.Parameters.AddWithValue("@mode", "Add");
+                        sqlCmd.Parameters.AddWithValue("@pid", txtPatientId.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@fname", txtFirstName.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@lname", txtLastName.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@age", txtAge.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@street", txtStreet.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@city", txtCity.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@zipcode", txtZipCode.Text.Trim());
+                        sqlCmd.ExecuteNonQuery();
+                        MessageBox.Show("Added,and Saved Suuccsessfully");
+                    }
+                    else
+                    {
+                        SqlCommand sqlCmd = new SqlCommand("Insert", sqlCon);
+                        sqlCmd.CommandType = CommandType.StoredProcedure;
+                        sqlCmd.Parameters.AddWithValue("@mode", "Edit");
+                        sqlCmd.Parameters.AddWithValue("@pid", PatientId);
+                        sqlCmd.Parameters.AddWithValue("@fname", txtFirstName.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@lname", txtLastName.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@age", txtAge.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@street", txtStreet.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@city", txtCity.Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@zipcode", txtZipCode.Text.Trim());
+                        sqlCmd.ExecuteNonQuery();
+                        MessageBox.Show("Updated Suuccsessfully");
+
+                    }
+                    
+
+                }
+                Reset();
+                fillDataGridView();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error,Can't perform the requested Insert Function");
+
+            }
+            finally
+            {
+                sqlCon.Close();
+
+            }
+
+
+        }
+        void fillDataGridView()
+        {
+            if (sqlCon.State == ConnectionState.Closed) {
+
+                sqlCon.Open();
+
+                SqlDataAdapter sqlData = new SqlDataAdapter("Search",sqlCon);
+
+                sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlData.SelectCommand.Parameters.AddWithValue("@fname", txtSearch.Text.Trim());
+                DataTable dtbl = new DataTable();
+                sqlData.Fill(dtbl);
+                dgvPatientInfo.DataSource = dtbl;
+                dgvPatientInfo.Columns[0].Visible = false;
+                sqlCon.Close();
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Reset();
+            fillDataGridView();
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                fillDataGridView();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error,Unable to perform the Search");
+            }
+
+        }
+
+        private void dgvPatientInfo_ReadOnlyChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvPatientInfo_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgvPatientInfo.CurrentRow.Index !=-1)
+            {
+                PatientId = Convert.ToInt32(dgvPatientInfo.CurrentRow.Cells[0].Value.ToString());
+                txtFirstName.Text = dgvPatientInfo.CurrentRow.Cells[1].Value.ToString();
+                txtLastName.Text = dgvPatientInfo.CurrentRow.Cells[2].Value.ToString();
+                txtAge.Text = dgvPatientInfo.CurrentRow.Cells[3].Value.ToString();
+                txtStreet.Text = dgvPatientInfo.CurrentRow.Cells[4].Value.ToString();
+                txtCity.Text = dgvPatientInfo.CurrentRow.Cells[5].Value.ToString();
+                txtZipCode.Text = dgvPatientInfo.CurrentRow.Cells[6].Value.ToString();
+                btnInsert.Text = "Update";
+                btnDelete.Enabled = true;
+
+
+            }
+
+        }
+        void Reset()
+        {
+            txtFirstName.Text = txtLastName.Text = txtAge.Text = txtCity.Text = txtStreet.Text = txtZipCode.Text = "";
+            btnInsert.Text = "Insert";
+            PatientId = 0;
+            btnDelete.Enabled = false;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                    SqlCommand sqlCmd = new SqlCommand("Delete", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@pid", PatientId);
+                    sqlCmd.ExecuteNonQuery();
+                    MessageBox.Show("Delete Operation Was Suuccsessful");
+                    Reset();
+                    fillDataGridView();
+
+
+                }
+            }catch(Exception ex){
+                MessageBox.Show(ex.Message,"Delete Operation Was Not Successful");
+
+            }
+        }
+    }
+}
